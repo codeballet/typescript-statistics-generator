3308921f780d55d0310f04bbb1f2922eda2a1790
johan <stjernholm@msn.com>
2020-09-11 12:31:07 +0800

    Feature: add static methods

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,35 @@
 import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReports';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
 
 // Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
+// const csvFileReader = new CsvFileReader('football.csv');
 
-// Create an instance of MatchReader and pass in something
-// satisfying the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+/*
+ * Create an instance of MatchReader and pass in something
+ * satisfying the 'DataReader' interface
+ */
+// const matchReader = new MatchReader(csvFileReader);
+// matchReader.load();
+
+/*
+ * Combine the above two calls with a preconfigured
+ * static method (without using 'new')
+ * Static methods can be used on the class itself,
+ * without having to first create an instance
+ */
+const matchReader = MatchReader.fromCsv('football.csv');
+
+// const summary = new Summary(
+//   new WinsAnalysis('Man United'),
+//   new ConsoleReport()
+// );
+
+// Using the static method in Summary (do not use 'new')
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
 matchReader.load();
-
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
